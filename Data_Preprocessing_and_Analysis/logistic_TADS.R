59917f5fa9a591d333a80fb75856b7dce60cc853
bingkaiwang <bingkai.w@gmail.com>
2017-10-23 17:12:41 -0400

    add comment

--- Data_Preprocessing_and_Analysis/logistic_TADS.R
+++ Data_Preprocessing_and_Analysis/logistic_TADS.R
@@ -1,3 +1,4 @@
+library(dplyr)
 load("Data_Preprocessing_and_Analysis/TADS.rdata")
 tad <- subset(tad, !is.na(tad$binary_CGI_improvement) & (tad$treatment %in% c("FLX", "PBO")))
 tad$treatment = tad$treatment == "FLX"
@@ -13,10 +14,13 @@ risk_diff <- data.frame(unadjusted = rep(NA, sim_size), standadized = rep(NA, si
 log_odds <- data.frame(unadjusted = rep(NA, sim_size), standardied = rep(NA, sim_size), 
                        logistic = rep(NA, sim_size))
 for(i in 1: sim_size){
+  # generate data
   stad <- sample_n(tad, size = n, replace = T)
   stad$treatment <- runif(n) < 0.5
   indi <- (stad$binary_CGI_improvement == 0) & (stad$treatment == 1)
   stad$binary_CGI_improvement[indi] <- as.integer(runif(sum(indi)) < q)
+  
+  # estimation
   glm_result <- glm(binary_CGI_improvement ~ . , data = stad, family = "binomial")
   log_odds$logistic[i] <- glm_result$coefficients[2]
   pred_p1 <- predict(glm_result, cbind(treatment = T, stad[,-(1:2)]), type = "response") %>% mean
@@ -28,13 +32,14 @@ for(i in 1: sim_size){
   risk_diff$unadjusted[i] = sp1 -sp0
   log_odds$unadjusted[i] = log(sp1/(1-sp1)) - log(sp0/(1-sp0))
 }
+
+# generate summary table
 risk_result <- matrix(NA, nrow = 3, ncol = 2, 
                       dimnames = list(c("mean", "s.e.", "RE"),
                                       c("unadjusted", "standardized")))
 logodds_result <- matrix(NA, nrow = 3, ncol = 3, 
                          dimnames = list(c("mean", "s.e.", "RE"),
                                          c("unadjusted", "standardized", "logistic")))
-
 logodds_result[1,] <- colMeans(log_odds)
 logodds_result[2,] <- cov(log_odds) %>% diag %>% sqrt
 logodds_result[3,] <- logodds_result[1,]^2/logodds_result[2,]^2/(logodds_result[1,3]^2/logodds_result[2,3]^2)
